b11a0aa59f4654f1ad5f643e201de8c724214b08
syun <n-nax2seo@outlook.jp>
2022-06-02 00:36:58 +0900

    6.2作業

---
+++ ThanksCardClient/ViewModels/HomeViewModel.cs
@@ -0,0 +1,38 @@
+﻿#nullable disable
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThanksCardClient.Models;
+using ThanksCardClient.Services;
+
+namespace ThanksCardClient.ViewModels
+{
+    internal class HomeViewModel : BindableBase
+    {
+        private readonly IRegionManager regionManager;
+
+        private Employee _AuthorizedEmployee;
+        public Employee AuthorizedEmployee
+        {
+            get { return _AuthorizedEmployee; }
+            set { SetProperty(ref _AuthorizedEmployee, value); }
+        }
+
+        #region LogoutCommand
+        private DelegateCommand _logoutCommand;
+        public DelegateCommand LogoutCommand =>
+            _logoutCommand ?? (_logoutCommand = new DelegateCommand(ExecuteLogoutCommand));
+
+        void ExecuteLogoutCommand()
+        {
+            SessionService.Instance.AuthorizedEmployee = null;
+            SessionService.Instance.IsAuthorized = false;
+
+            this.regionManager.RequestNavigate("ContentRegion", nameof(Views.Logon));
+        }
+        #endregion
+    }
+}
